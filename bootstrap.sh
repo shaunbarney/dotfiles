4b00ad7697bb3d78e78ebe3ba122c34a0bf2e856
Zihao Zhang <zzh8829@gmail.com>
2014-10-31 01:29:21 -0400

    random stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".osx" \
+		--exclude "misc/" --exclude "README.md" --exclude "LICENSE-MIT.txt"
 		-avh --no-perms . ~;
 }
 
