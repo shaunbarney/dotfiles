41d59f9ed8a9fe7a202ddde8c167f54ee143311a
Zihao Zhang <zzh8829@gmail.com>
2019-06-26 11:04:46 +0100

    linux fixes

--- github_scripts.sh
+++ github_scripts.sh
@@ -2,3 +2,5 @@
 
 mkdir ~/.zsh-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
